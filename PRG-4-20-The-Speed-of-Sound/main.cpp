72b1c3d333da3d0758b4cb4e17f8ca63cf4bb963
Keith Smith <aeiouandsometimesy@me.com>
2017-10-07 11:32:58 -0400

    Initial Commit

---
+++ PRG-4-20-The-Speed-of-Sound/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PRG-4-20-The-Speed-of-Sound
+//
+//  Created by Keith Smith on 10/7/17.
+//  Copyright © 2017 Keith Smith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
